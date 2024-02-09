33c0362b44009e93376134762025c790a7b48f18
Pietro Pasotti <starfire.daemon@gmail.com>
2024-02-09 12:29:13 +0100

    broad test on e2e endpoint names

--- tests/unit/test_e2e.py
+++ tests/unit/test_e2e.py
@@ -5,17 +5,17 @@ from textwrap import dedent
 import pytest
 from ops import CharmBase
 from scenario import State
-from utils import CRI_LIKE_PATH
 
 from interface_tester import InterfaceTester
 from interface_tester.collector import gather_test_spec_for_version
-from interface_tester.errors import SchemaValidationError
+from interface_tester.errors import SchemaValidationError, InvalidTestCaseError
 from interface_tester.interface_test import (
     InvalidTesterRunError,
     NoSchemaError,
     NoTesterInstanceError,
     Tester,
 )
+from utils import CRI_LIKE_PATH
 
 
 class LocalTester(InterfaceTester):
@@ -122,6 +122,35 @@ def test_data_on_changed():
 
     with pytest.raises(InvalidTesterRunError):
         tester.run()
+
+
+def test_error_if_not_relation_event():
+    tester = _setup_with_test_file(
+        dedent(
+            """
+from scenario import State, Relation
+
+from interface_tester.interface_test import Tester
+
+def test_data_on_changed():
+    t = Tester(State(
+        relations=[Relation(
+            endpoint='foobadooble',  # should not matter
+            interface='tracing',
+            remote_app_name='remote',
+            local_app_data={}
+        )]
+    ))
+    t.run("foobadooble-changed")
+    t.skip_schema_validation()
+"""
+        )
+    )
+
+    with pytest.raises(InvalidTestCaseError) as e:
+        tester.run()
+
+    assert e.match("Bad interface test specification: event foobadooble-changed is not a relation event.")
 
 
 def test_error_if_assert_relation_data_empty_before_run():
@@ -275,10 +304,21 @@ def test_data_on_changed():
         tester.run()
 
 
-def test_valid_run():
+@pytest.mark.parametrize("endpoint", (
+        "foo-one",
+        "prometheus-scrape",
+        "foobadoodle",
+        "foo-one-two"))
+@pytest.mark.parametrize("evt_type", (
+        "changed",
+        "created",
+        "joined",
+        "departed",
+        "broken"))
+def test_valid_run(endpoint, evt_type):
     tester = _setup_with_test_file(
         dedent(
-            """
+            f"""
  from scenario import State, Relation
 
  from interface_tester.interface_test import Tester
@@ -287,13 +327,13 @@ def test_valid_run():
  def test_data_on_changed():
      t = Tester(State(
          relations=[Relation(
-             endpoint='foobadooble',  # should not matter
+             endpoint='{endpoint}',  # should not matter
              interface='tracing',
              remote_app_name='remote',
-             local_app_data={}
+             local_app_data={{}}
          )]
      ))
-     state_out = t.run("axolotl-relation-changed")
+     state_out = t.run("{endpoint}-relation-{evt_type}")
      t.assert_schema_valid(schema=DataBagSchema())
  """
         )
